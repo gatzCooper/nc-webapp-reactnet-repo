e4052635d6145e7055fe60f03f39e220394933dd
mond gatacelo <raymundo.gatacelo@ing.com>
2023-05-09 21:14:01 +0800

    enable swagger

--- nc-attendance-app-api/Program.cs
+++ nc-attendance-app-api/Program.cs
@@ -55,11 +55,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseCors(corsPolicy);
 
